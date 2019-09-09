9bd9bddfb978fe50f17a6bb0d74dd5d714caa85b
DAH 01 <dah01@FORMATION.lan>
2019-09-09 17:11:53 +0200

    Nettoyage App

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
-import Mapbox from '@react-native-mapbox-gl/maps';
 import Entete from './app/components/Entete';
 import Map from './app/components/Map';
-Mapbox.setAccessToken(
-	'pk.eyJ1IjoicGFjdGVzcyIsImEiOiJjazBjNmsydmMweXJlM21wZTh6NGl4cml3In0.QtCHsu5sWL6tKXP6so4bbA'
-);
+
 export default class App extends Component {
 	render() {
 		return (
