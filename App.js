8f955e0f8470914c50d6a520f9f08203719d5e33
DAH 04 <dah04@FORMATION.lan>
2019-09-09 11:27:33 +0200

    mise en place map

--- App.js
+++ App.js
@@ -1,27 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Mapbox from '@react-native-mapbox-gl/maps';
 
-import React, {Fragment} from 'react';
-import Map from './app/components/Map';
-import {
-  StatusBar,
-  Text
-} from 'react-native';
+Mapbox.setAccessToken(
+	'UseYourMapboxTokenHere'
+);
+export default class App extends Component {
+	renderAnnotations() {
+		return (
+			<Mapbox.PointAnnotation
+				key="pointAnnotation"
+				id="pointAnnotation"
+				coordinate={[11.254, 43.772]}>
+				<View style={styles.annotationContainer}>
+					<View style={styles.annotationFill} />
+				</View>
+				<Mapbox.Callout title="An annotation here!" />
+			</Mapbox.PointAnnotation>
+		);
+	}
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <Map/>
-      <Text>Test</Text>
-    </Fragment>
-  );
-};
+	render() {
+		return (
+			<View style={styles.container}>
+				<Mapbox.MapView
+					styleURL={Mapbox.StyleURL.Street}
+					style={styles.container}>
+              <Mapbox.Camera
+                zoomLevel={8}
+                centerCoordinate={[11.256, 43.77]}
+              />
 
+					{this.renderAnnotations()}
+				</Mapbox.MapView>
+			</View>
+		);
+	}
+}
 
-export default App;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	map: {
+		height: 400,
+		marginTop: 80
+	},
+	annotationContainer: {
+		width: 30,
+		height: 30,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'white',
+		borderRadius: 15
+	},
+	annotationFill: {
+		width: 30,
+		height: 30,
+		borderRadius: 15,
+		backgroundColor: 'blue',
+		transform: [{ scale: 0.6 }]
+	}
+});
