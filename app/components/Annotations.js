533995296d23364ff90134ccb70e587973cae945
DAH 01 <dah01@FORMATION.lan>
2019-09-13 17:05:05 +0200

    test popupcarte

--- app/components/Annotations.js
+++ app/components/Annotations.js
@@ -3,27 +3,36 @@ import { View, StyleSheet, Text } from 'react-native';
 import Mapbox from '@react-native-mapbox-gl/maps';
 import PLACES from '../consts/Places';
 import PopupCarte from './PopupCarte';
+import FicheDetail from './FicheDetail';
 
 export default class Annotations extends Component {
   constructor(){
     super();
     this.state = {
       items : PLACES,
+      page : 'carte'
     }
   }
   render (){
     let items = this.state.items
   return (
-    <View>
+
+    (this.state.page === 'carte') ?
+    <View style={styles.container}>
       {items.map((item,k) => 
     <Mapbox.PointAnnotation
     key={k}
     id={'PointAnnotation'+k}
     coordinate={item.coordonnees}>
     <Mapbox.Callout>
-      <PopupCarte donnees={item} info={this.props.popup}/>
+      <PopupCarte donnees={item} info={() => this.setState({page:'detail'})}/>
     </Mapbox.Callout>
-    </Mapbox.PointAnnotation>)}
+    </Mapbox.PointAnnotation>
+    )}
+  </View>
+  :
+  <View style={styles.container}>
+    <FicheDetail style={styles.fiche}/>
   </View>
   )}
 }
@@ -32,28 +41,8 @@ export default class Annotations extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1
-	},
-	map: {
-		height: 400,
-		marginTop: 80
-	},
-	annotationContainer: {
-		width: 30,
-		height: 30,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: 'white',
-		borderRadius: 15
-	},
-	annotationFill: {
-		width: 30,
-		height: 30,
-		borderRadius: 15,
-		backgroundColor: 'blue',
-		transform: [{ scale: 0.6 }]
   },
-  infoText:{
-    textAlign:'left',
-
+  fiche: {
+    width:400
   }
 });
--- app/components/PopupCarte.js
+++ app/components/PopupCarte.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import FicheDetail from './FicheDetail';
 
 
 export default class PopupCarte extends Component {
+  constructor(){
+    super();
+    this.state = {
+      page : 'carte',
+    }
+  }
+
   render () {
     let item = this.props.donnees;
     return(
-      
+      (this.state.page === 'carte') ?
       <View style={styles.container}>
         <Text style={styles.societe}>{item.societe}</Text>
         <Text style={styles.type}>{item.type}</Text>
         <Text>{item.adresse}</Text>
         <Text>{item.codepostal} {item.ville.toUpperCase()}</Text>
-        <TouchableOpacity onPress={this.props.info}><Text style={styles.info}>Plus d'infos...</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => this.setState({page:'detail'})}><Text style={styles.info}>Plus d'infos...</Text></TouchableOpacity>
       </View>
-      
+      :
+      <View>
+      <FicheDetail style={styles.fiche}/>
+      </View>
     )
   }
 }
@@ -39,5 +50,9 @@ const styles = StyleSheet.create({
   info : {
     color:'blue',
     textAlign:'right'
+  },
+  fiche: {
+    position:'absolute',
+    top:10
   }
 });
