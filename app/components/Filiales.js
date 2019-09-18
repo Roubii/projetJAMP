7acd719c0fc39b5aa80f1297c79ea879ab9f52cb
DAH 07 <dah07@FORMATION.lan>
2019-09-18 10:42:06 +0200

    Maj fiche detail

--- app/components/Filiales.js
+++ app/components/Filiales.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, StyleSheet, Image, ScrollView } from 'react-native';
 
 
 
-export default class Filiales extends Component {
-  constructor () {
-    super();
-    this.state ={
-
-    }
-  }
-	
-	render() {
+const Filiales = (props) => {
+    if (props.group === 'additi'){
 		return (
-<View style={styles.contentContainer}>
-    <ScrollView  >
-      <TouchableOpacity><Image style={styles.image}source={require('../img/INFOMER/logo-500x172_chasse-maree.jpg')} /></TouchableOpacity>
-      <TouchableOpacity><Image style={styles.image}source={require('../img/PRECOM/logo-500x172_precom-alimentation.jpg')} /></TouchableOpacity>
-      <TouchableOpacity><Image style={styles.image}source={require('../img/REGIE-RADIOS-REGION/logo-500x172_cocktail-fm.jpg')} /></TouchableOpacity>
-      <TouchableOpacity><Image style={styles.image}source={require('../img/logo-500x172_adrexo.jpg')} /></TouchableOpacity>
-      <TouchableOpacity><Image style={styles.image}source={require('../img/JOURNAUX-DE-LOIRE/logo-500x172_courrier-de-l-ouest.jpg')} /></TouchableOpacity>
-    </ScrollView>
-    </View>
+      <View style={styles.contentContainer}>
+        <ScrollView >
+          <Image style={styles.image}source={require('../img/PRECOM/logo-500x172_precom-alimentation.jpg')} />
+          <Image style={styles.image}source={require('../img/REGIE-RADIOS-REGION/logo-500x172_cocktail-fm.jpg')} />
+          <Image style={styles.image}source={require('../img/Medialex.png')} />
+          <Image style={styles.image}source={require('../img/Happy Prod.png')} />
+          <Image style={styles.image}source={require('../img/Precontact.png')} />
+          <Image style={styles.image}source={require('../img/logo-500x172_ouest-france-multimedia.jpg')} />
+          <Image style={styles.image}source={require('../img/nantesMedia.png')} />
+          <Image style={styles.image}source={require('../img/Dans_nos_cœurs.jpg')} />
+          <Image style={styles.image}source={require('../img/Affiouest.jpg')} />
+        </ScrollView>
+      </View>
+
 		);
-	}
+	} else if(props.group === 'ouestfrance') {
+    return (
+      <View style={styles.contentContainer}>
+        <ScrollView >
+          <Image style={styles.image}source={require('../img/logo-500x172_OF_Rouge.jpg')} />
+          <Image style={styles.image}source={require('../img/INFOMER/logo-500x172_chasse-maree.jpg')} />
+          <Image style={styles.image}source={require('../img/INFOMER/logo-500x172_le-marin.jpg')} />
+          <Image style={styles.image}source={require('../img/logo-pdm.png')} />
+          <Image style={styles.image}source={require('../img/INFOMER/logo-500x172_chasse-maree.jpg')} />
+          <Image style={styles.image}source={require('../img/logo-500x172_voiles-et-voiliers.jpg')} />
+          <Image style={styles.image}source={require('../img/logo-500x172_editions-ouest-france.jpg')} />
+        </ScrollView>
+      </View>
+      );
+  }
 }
+
+export default Filiales;
 
 const styles = StyleSheet.create({
 	contentContainer:{
