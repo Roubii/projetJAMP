bc131c50d20d0a1bc14523d8dcae010fe145d8b2
DAH 07 <dah07@FORMATION.lan>
2019-09-20 10:11:46 +0200

    Maj Home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,31 +1,36 @@
 import React, {Component} from  'react';
-import { StyleSheet, View, Image, Text, TouchableOpacity, ScrollView} from 'react-native';
+import { StyleSheet, View, Image,ImageBackground, Text, TouchableOpacity, ScrollView} from 'react-native';
+
 
 export default class Home extends Component {
   render () {
     return(
-      
+     
       <View style={styles.main}>
-        <Text style={styles.texte}>Les différents groupes appartenant à SIPA Ouest-France :</Text>
+        <Text style={styles.texte}>Les différents groupes appartenant à SIPA Ouest-France</Text>
         <ScrollView contentContainerStyle={styles.container} horizontal={true} showsHorizontalScrollIndicator>
           <View style={styles.blocLogo}>
           <TouchableOpacity style={styles.containertouchable} onPress={this.props.OuestFrance}><Image source={require('../img/logo-500x172_OF_Rouge.jpg')} style={styles.logo}/></TouchableOpacity>
           <TouchableOpacity style={styles.containertouchable} onPress={this.props.Additi}><Image source={require('../img/Additi-logo.gif')} style={styles.logo}/></TouchableOpacity>                   
           </View>
         </ScrollView>
+        <ImageBackground source={require('../img/brittany-1146351_960_720.jpg')} style={{position:"absolute", top:0, left:0,right:0, bottom:0, zIndex:-1}}></ImageBackground>
       </View>
+    
     )
   }
 }
+
 
 const styles = StyleSheet.create({
   main: {
     flex:1,
     backgroundColor:'white',
-    width:'100%',
+    width:'100%'
   },
   container:{
     width:'100%',
+    alignItems:'flex-end'
   },
   blocLogo:{  
     flex:1,  
@@ -33,19 +38,24 @@ const styles = StyleSheet.create({
     justifyContent:'space-around',
     flexDirection:'row',
     alignItems:'center',
+    elevation:2,
+    height:'20%',
+    backgroundColor:'rgb(255,255,255)'
   },
   logo:{
-    width:400,
-    resizeMode:'cover',
+    width:300,
+    height:'80%',
+    resizeMode:'cover', 
   },
   texte: {
     fontSize:30,
     textAlign:'center',
-    padding:30,
+    padding:10,
+    backgroundColor:'rgba(255,255,255,0.4)',
+    color:'#083E98'
   },
   containertouchable:{
     height:150,
-    marginBottom:'10%',
-   
+    justifyContent:'center'
   }
 })
